6786c972cb7240ac73a94e0a420c6033a33a5a6d
Shubham <shubhpatel108@gmail.com>
2013-07-27 14:01:42 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PicUp::Application.config.secret_key_base = 'acead0ee6fc8c4cfed1d64cf6399bc6a72a11bdfd9a904acf9b375b3c824e9f837cb9209db007c95aa88d2e9c08c50cd4620f5b58b790fbec4083ad9d90dbc9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PicUp::Application.config.session_store :cookie_store, key: '_pic_up_session'
