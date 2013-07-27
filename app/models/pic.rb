46cebc65b8c11a9a41c9284a1c2ab252c578a776
Shubham <shubhpatel108@gmail.com>
2013-07-27 15:40:15 +0530

    Delete back from new pic viem and Solve problem

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,3 +1,11 @@
 class Pic < ActiveRecord::Base
-  # attr_accessible :title, :body
+  
+  attr_accessible :name, :cation, :picture
+  validates_presence_of :name, :picture
+
+  has_attached_file :picture, :style =>{:thumb => "50x50#", 
+  										:small => "150x150",
+  										:medium => "300x300",
+  										:large => "400x400"}
+
 end
--- db/migrate/20130727090106_create_pics.rb
+++ db/migrate/20130727090106_create_pics.rb
@@ -2,7 +2,7 @@ class CreatePics < ActiveRecord::Migration
   def change
     create_table :pics do |t|
     	t.string :name
-    	t.string :cation
+    	t.string :caption
     	t.has_attached_file :picture
       t.timestamps
     end
